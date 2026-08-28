111c63f81c8266a19a67b65ac2217e83547142b9
ArcheDH <archedh@outlook.com>
2026-08-28 14:11:55 -0400

    Cleaned up code for readaility.

--- Assignment 1 - Java Review + Algorithm Efficiency/Dice.java
+++ Assignment 1 - Java Review + Algorithm Efficiency/Dice.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.Arrays;
 import java.util.Random;
 
 public class Dice{
@@ -13,7 +12,6 @@ public class Dice{
         minimumFace = 0;
         minimumStreak = 0;
         streakCount = new int[0];
-
     }
     public static void main(String[] args) {
         System.out.println("Welcome to my dice roller!"); // Welcome message.
@@ -28,47 +26,44 @@ public class Dice{
         int[] rolls; // All rolls done.
 
         while(check == true){ // Loop while the loop variable is true.
-         System.out.print("How many rounds would you like to roll: ");
-        int numRounds = myScanner.nextInt();
+            System.out.print("How many rounds would you like to roll: ");
+            int numRounds = myScanner.nextInt();
 
-        System.out.print("What is the minimum die value: ");
-        myDice.setMinimumFace(myScanner.nextInt());
+            System.out.print("What is the minimum die value: ");
+            myDice.setMinimumFace(myScanner.nextInt());
 
-        System.out.print("How many faces would you like: ");
-        myDice.setNumFaces(myScanner.nextInt() + myDice.minimumFace);
+            System.out.print("How many faces would you like: ");
+            myDice.setNumFaces(myScanner.nextInt() + myDice.minimumFace);
         
-        System.out.print("How many times would you like to roll the die: ");
-        numRolls = myScanner.nextInt();
-        rolls = new int[numRolls];
+            System.out.print("How many times would you like to roll the die: ");
+            numRolls = myScanner.nextInt();
+            rolls = new int[numRolls];
         
-        System.out.print("Is there a threshold you only want to count for (y/n): ");
-        thresholdCheck = myScanner.next();
+            System.out.print("Is there a threshold you only want to count for (y/n): ");
+            thresholdCheck = myScanner.nextLine();
 
-        if (thresholdCheck == "y"){
-            check = true;
-            while (check == true){
-            System.out.print("What is the number: ");
-
-            myDice.setMinimumStreak(myScanner.nextInt());
-
-                if (myDice.getMinimumStreak() > (myDice.getMinimumFace() + myDice.getNumFaces())){
-                System.out.println("Number too big! Try again!");
+           if (thresholdCheck == "y"){
+                check = true;
+                while (check == true){
+                    System.out.print("What is the number: ");
+                    myDice.setMinimumStreak(myScanner.nextInt());
+                        if (myDice.getMinimumStreak() > (myDice.getMinimumFace() + myDice.getNumFaces())){
+                            System.out.println("Number too big! Try again!");
+                        }
+                        else{
+                            check = false;
+                        }
                 }
-                else{
-                check = false;
-                }
-
-            }}
-        else {
-            myDice.setMinimumStreak(myDice.minimumFace);
-        }
-
-        for (int i = 0; i < numRolls; i++ ){
-            randomNumber = randomInt.nextInt(myDice.minimumFace + 1, (myDice.minimumFace + myDice.numFaces) + 1); // Generate a random number that the dice can roll.
-            if (randomNumber >= myDice.getMinimumStreak()){ // Check if the number is greater than the specified threshold.
-                myDice.addStreak(rolls, randomNumber); // Send to addStreak method.
             }
-        }
+            else {
+                myDice.setMinimumStreak(myDice.minimumFace);
+            }
+            for (int i = 0; i < numRolls; i++ ){
+                randomNumber = randomInt.nextInt(myDice.minimumFace + 1, (myDice.minimumFace + myDice.numFaces) + 1); // Generate a random number that the dice can roll.
+                if (randomNumber >= myDice.getMinimumStreak()){ // Check if the number is greater than the specified threshold.
+                    myDice.addStreak(rolls, randomNumber); // Send to addStreak method.
+                }
+            }
         }
     }
     // Getter methods.
@@ -79,16 +74,13 @@ public class Dice{
     public void setNumFaces(int num){numFaces = num;}           // Get the value in numFaces
     public void setMinimumFace(int num){minimumFace = num;}     // Get the value in minimumFace.
     public void setMinimumStreak(int num){minimumStreak = num;} // Get the value in minimumStreak.
-
     // Add a number to the streak array.
     public int[] addStreak(int[] array, int number){
-        int[] newArray = new int[array.length + 1];
-        for (int i = 0; i < array.length; i++){
+        int[] newArray = new int[array.length + 1]; // Make a new array with a length one greater than the original.
+        for (int i = 0; i < array.length; i++){ // Iterate through the new array adding values from the last one.
             newArray[i] = array[i];
         }
-        newArray[newArray.length - 1] = number;
-        
+        newArray[newArray.length - 1] = number; // Add the new value.
         return newArray;
     }
-
 }
